c71bc6066951fe419079c13880a600d2e5460833
LuisTheAnt <val.alvarezr@duocuc.cl>
2025-03-31 21:13:28 -0300

    Usuario DTO
    atributos y correción.

--- src/main/java/cl/ecommerce/ecommerce/dto/UsuarioDTO.java
+++ src/main/java/cl/ecommerce/ecommerce/dto/UsuarioDTO.java
@@ -1,5 +1,79 @@
 package cl.ecommerce.ecommerce.dto;
 
-public interface UsuarioDTO {
+public class UsuarioDTO {
+    private int rut;
+    private String nombres;
+    private String apPaterno;
+    private String apMaterno;
+    private String fechaNacimiento;
+    private String email;
+    private String hashContraseña;
+
+    public UsuarioDTO(int rut, String nombres, String apPaterno, String apMaterno, String fechaNacimiento, String email,
+            String hashContraseña) {
+        this.rut = rut;
+        this.nombres = nombres;
+        this.apPaterno = apPaterno;
+        this.apMaterno = apMaterno;
+        this.fechaNacimiento = fechaNacimiento;
+        this.email = email;
+        this.hashContraseña = hashContraseña;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashContraseña() {
+        return hashContraseña;
+    }
+
+    public void setHashContraseña(String hashContraseña) {
+        this.hashContraseña = hashContraseña;
+    }
 
 }
