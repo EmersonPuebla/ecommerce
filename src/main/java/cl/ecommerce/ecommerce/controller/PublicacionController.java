6b26f4d4354faea93eb250a08cf4bfbc56afe49a
EmersonPuebla <em.puebla@duocuc.cl>
2025-04-09 11:56:38 -0400

    getAll añadido al PublicacionController
    Co-Authored-By: LuisTheAnt <188342499+LuisTheAnt@users.noreply.github.com>

--- src/main/java/cl/ecommerce/ecommerce/controller/PublicacionController.java
+++ src/main/java/cl/ecommerce/ecommerce/controller/PublicacionController.java
@@ -1,12 +1,33 @@
 package cl.ecommerce.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import cl.ecommerce.ecommerce.dto.PublicacionDTO;
+import cl.ecommerce.ecommerce.service.impl.PublicacionServiceImpl;
 
 @RestController
-
 @RequestMapping("/api/crud/publicaciones")
 public class PublicacionController {
+
+    @Autowired
+    private PublicacionServiceImpl publicacionService;
+
+    @GetMapping
+    public ResponseEntity<List<PublicacionDTO>> getAll(@PathVariable Integer id) {
+        List<PublicacionDTO> publicaciones = publicacionService.findAll();
+        if (publicaciones.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(publicaciones);
+    }
+    
+
 
 }
