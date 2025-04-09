7b5cfeee9ae6a8556984e9168588823eeea2a3ee
EmersonPuebla <em.puebla@duocuc.cl>
2025-04-09 12:48:14 -0400

    Cambios de tipo de datos en la base de datos
    Co-Authored-By: LuisTheAnt <188342499+LuisTheAnt@users.noreply.github.com>

--- src/main/java/cl/ecommerce/ecommerce/dto/UsuarioDTO.java
+++ src/main/java/cl/ecommerce/ecommerce/dto/UsuarioDTO.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +24,7 @@ public class UsuarioDTO {
     private Integer run;
 
     @Column
-    (nullable = false)
+    (nullable = false, columnDefinition = "NUMBER(3)")
     private Integer dv;
 
     @Column(nullable=false) 
@@ -35,6 +37,7 @@ public class UsuarioDTO {
     private String apmaterno;
 
     @Column(nullable=false)  
+    @Temporal(TemporalType.DATE)
     private Date fechaNacimiento;
 
     @Column(nullable=false) 
