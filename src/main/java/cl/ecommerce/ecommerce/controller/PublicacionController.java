9c5cb61c4c2913a758d9c00129d04e4787fcdaa9
LuisTheAnt <val.alvarezr@duocuc.cl>
2025-04-09 00:32:40 -0400

    PUBLICACION & ESTADO PUBLICACION
    3f62d7cc1cc5ce187689c14fc15134b90f13d2f0

--- src/main/java/cl/ecommerce/ecommerce/controller/PublicacionController.java
+++ src/main/java/cl/ecommerce/ecommerce/controller/PublicacionController.java
@@ -1,5 +1,12 @@
 package cl.ecommerce.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+
+@RequestMapping("/api/crud/publicaciones")
 public class PublicacionController {
 
 }
