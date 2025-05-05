e41f3168d53d371fa76ed98512b6518bc64d061f
EmersonPuebla <em.puebla@duocuc.cl>
2025-05-05 14:53:06 -0400

    Categoria Ctrl & Service Added

--- src/main/java/cl/ecommerce/ecommerce/controller/CategoriaController.java
+++ src/main/java/cl/ecommerce/ecommerce/controller/CategoriaController.java
@@ -1,5 +1,85 @@
 package cl.ecommerce.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.ecommerce.ecommerce.dto.CategoriaDTO;
+import cl.ecommerce.ecommerce.service.impl.CategoriaServiceImpl;
+
+@RestController
+@RequestMapping("/api/crud/categorias")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaServiceImpl categoriaService;
+
+    @GetMapping 
+    public ResponseEntity<List<CategoriaDTO>> getAll() {
+        List<CategoriaDTO> categorias = categoriaService.findAll();
+
+        if (categorias.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(categorias);
+    }
+
+   @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> getById(@PathVariable Integer id) {
+        CategoriaDTO categoria = categoriaService.findById(id);
+
+        if (categoria == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(categoria);
+    }
+
+     @PostMapping
+    public ResponseEntity<?> insertCategoria(@RequestBody CategoriaDTO categoria) {
+
+        if (categoriaService.findById(categoria.getId()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);         
+        }
+
+        CategoriaDTO newCategoria = categoriaService.save(categoria);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCategoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> updateCategoria(@PathVariable Integer id, @RequestBody CategoriaDTO categoria) {
+        if (categoriaService.findById(id) == null) {
+            return ResponseEntity.notFound().build();
+        }
+    
+        CategoriaDTO newCategoria = categoriaService.update(id, categoria);
+
+        if (newCategoria != null) {
+            return ResponseEntity.ok(categoria);        
+        }
+        
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+        try {
+            categoriaService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch ( Exception e ) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+    
 
 }
--- src/main/java/cl/ecommerce/ecommerce/service/ICategoriaService.java
+++ src/main/java/cl/ecommerce/ecommerce/service/ICategoriaService.java
@@ -1,5 +1,13 @@
 package cl.ecommerce.ecommerce.service;
 
-public interface ICategoriaService {
+import java.util.List;
 
+import cl.ecommerce.ecommerce.dto.CategoriaDTO;
+
+public interface ICategoriaService {
+    List<CategoriaDTO> findAll();
+    CategoriaDTO findById(Integer id);
+    CategoriaDTO save(CategoriaDTO producto);
+    CategoriaDTO update(Integer id, CategoriaDTO categoria);
+    void delete(Integer id);
 }
