a6950f9ee52c351fcdc18ea2014b3b8b78070633
EmersonPuebla <em.puebla@duocuc.cl>
2025-05-12 14:21:23 -0400

    ProductoDTO JOINEADO con PublicacionDTO
    Co-Authored-By: LuisTheAnt <188342499+LuisTheAnt@users.noreply.github.com>

--- src/main/java/cl/ecommerce/ecommerce/dto/PublicacionDTO.java
+++ src/main/java/cl/ecommerce/ecommerce/dto/PublicacionDTO.java
@@ -5,6 +5,8 @@ import java.sql.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +22,10 @@ public class PublicacionDTO {
 
     @Id
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private ProductoDTO producto;
 
     @Column(nullable = false)
     private Date fecha_publicacion;
