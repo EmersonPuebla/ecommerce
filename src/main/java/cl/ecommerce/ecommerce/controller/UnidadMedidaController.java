cc326974db410806e17c9e00b55aaece23d6c788
EmersonPuebla <em.puebla@duocuc.cl>
2025-05-19 15:45:17 -0400

    fix

--- src/main/java/cl/ecommerce/ecommerce/controller/UnidadMedidaController.java
+++ src/main/java/cl/ecommerce/ecommerce/controller/UnidadMedidaController.java
@@ -3,19 +3,16 @@ package cl.ecommerce.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.ecommerce.ecommerce.dto.UnidadMedidaDTO;
-import cl.ecommerce.ecommerce.dto.UsuarioDTO;
 import cl.ecommerce.ecommerce.service.impl.UnidadMedidaImpl;
 
 @RestController
