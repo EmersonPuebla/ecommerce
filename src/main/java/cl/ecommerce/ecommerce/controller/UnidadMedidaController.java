5cfa80cb9e86a4a976c937ea4cde067479255392
LuisTheAnt <val.alvarezr@duocuc.cl>
2025-05-19 15:22:59 -0400

    UnidadMedida - GetById & DeleteById

--- src/main/java/cl/ecommerce/ecommerce/controller/UnidadMedidaController.java
+++ src/main/java/cl/ecommerce/ecommerce/controller/UnidadMedidaController.java
@@ -3,21 +3,28 @@ package cl.ecommerce.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.ecommerce.ecommerce.dto.UnidadMedidaDTO;
+import cl.ecommerce.ecommerce.dto.UsuarioDTO;
 import cl.ecommerce.ecommerce.service.impl.UnidadMedidaImpl;
 
 @RestController
 @RequestMapping("/api/crud/unidadmedida")
+
 public class UnidadMedidaController {
 
     @Autowired
-    UnidadMedidaImpl unidadMedidaService;
+    private UnidadMedidaImpl unidadMedidaService;
 
     @GetMapping
     public ResponseEntity<List<UnidadMedidaDTO>> getAll(){
@@ -28,6 +35,48 @@ public class UnidadMedidaController {
         return ResponseEntity.ok(unidadMedida);
     }
 
- 
+    @GetMapping("/{id}")
+    public ResponseEntity<UnidadMedidaDTO> getById(@PathVariable Integer id){
+        UnidadMedidaDTO unidad_medida = unidadMedidaService.findById(id);
+        if (unidad_medida == null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(unidad_medida);
+    }
 
+    //@PostMapping
+    //public ResponseEntity<?> insertUnidadMedida(@RequestBody UnidadMedidaDTO unidad_medida){
+        
+    //if (unidadMedidaService.findById(unidad_medida.getId()) != null) {
+    //    return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+    //}
+    
+    //UnidadMedidaDTO newUnidadMedida = unidadMedidaService.save(unidad_medida);
+    //return ResponseEntity.status(HttpStatus.CREATED).body(newUnidadMedida);
+    //}
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UnidadMedidaDTO> updateUnidadMedida(@PathVariable Integer id, @RequestBody UnidadMedidaDTO unidad_medida) {
+        if (unidadMedidaService.findById(id) == null) {
+            return ResponseEntity.notFound().build();
+        }
+    
+        UnidadMedidaDTO newUnidadMedida = unidadMedidaService.update(id, unidad_medida);
+
+        if (newUnidadMedida != null) {
+            return ResponseEntity.ok(unidad_medida);        
+        }
+        
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+        try {
+            unidadMedidaService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch ( Exception e ) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
 }
