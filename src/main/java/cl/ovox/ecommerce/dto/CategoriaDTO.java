f2dfe950dc70cf50e3933d5b15a39b2e980308c5
EmersonPuebla <em.puebla@duocuc.cl>
2025-05-28 13:53:53 -0400

    Update CategoriaDTO.java

--- src/main/java/cl/ovox/ecommerce/dto/CategoriaDTO.java
+++ src/main/java/cl/ovox/ecommerce/dto/CategoriaDTO.java
@@ -1,11 +1,8 @@
 package cl.ovox.ecommerce.dto;
 
-import java.util.List;
-
 import cl.ovox.ecommerce.model.base.UUIDBaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +19,4 @@ public class CategoriaDTO extends UUIDBaseEntity {
     @Column(nullable = false)
     private String nombre;
 
-    @ManyToMany(mappedBy = "categorias")
-    private List<ProductoDTO> productos;
 }
